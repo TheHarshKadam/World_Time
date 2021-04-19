ceaaa66af67b32be09d29d51b7f7a1a6cd4b9a28
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-04-19 13:52:19 +0530

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,36 +6,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   Map data = {};
 
   @override
   Widget build(BuildContext context) {
-
     data = data.isNotEmpty ? data : ModalRoute.of(context).settings.arguments;
 
-    // set background image
     String bgImage = data['isMorning'] ? 'Day.jpeg' : 'Night.jpeg';
-    Color bgColor = data['isMorning'] ? Colors.blue : Colors.indigo[700];
-
+    Color bgColor = data['isMorning'] ? Colors.blueGrey[900] : Colors.blue[100];
+    
     return Scaffold(
       backgroundColor: bgColor,
       body: SafeArea(
         child: Container(
           decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/$bgImage'),
-              fit: BoxFit.cover,
-            )
-          ),
+              image: DecorationImage(
+            image: AssetImage('assets/$bgImage'),
+            fit: BoxFit.cover,
+          )),
           child: Padding(
             padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
             child: Column(
               children: <Widget>[
                 FlatButton.icon(
                   onPressed: () async {
-                    dynamic output = await Navigator.pushNamed(context, '/location');
-                    if(output != null){
+                    dynamic output =
+                        await Navigator.pushNamed(context, '/location');
+                    if (output != null) {
                       setState(() {
                         data = {
                           'time': output['time'],
@@ -72,13 +69,8 @@ class _HomeState extends State<Home> {
                   ],
                 ),
                 SizedBox(height: 20.0),
-                Text(
-                  data['time'],
-                  style: TextStyle(
-                    fontSize: 66.0,
-                    color: Colors.white
-                  )
-                ),
+                Text(data['time'],
+                    style: TextStyle(fontSize: 66.0, color: Colors.white)),
               ],
             ),
           ),
