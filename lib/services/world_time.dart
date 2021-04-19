88fd4015b52aa43c0a810b4ed474b67c768a26c9
Harshvardhan Kadam <77354987+TheHarshKadam@users.noreply.github.com>
2021-04-19 13:57:15 +0530

    Fixed boolean expression Error

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -13,21 +13,27 @@ class WorldTime {
 
   Future<void> getTime() async {
     try {
+      print('Getting Response...');
       Response response =
           await get(Uri.https('worldtimeapi.org', 'api/timezone/$url'));
-      Map data = jsonDecode(response.body);
+      print(response.statusCode);
+      if (response.statusCode == 200) {
+        Map data = jsonDecode(response.body);
 
-      String datetime = data['datetime'];
-      String offset = data['utc_offset'].substring(0, 3);
-      String offset_mnt = data['utc_offset'].substring(4, 6);
+        String datetime = data['datetime'];
+        String offset = data['utc_offset'].substring(0, 3);
+        String offset_mnt = data['utc_offset'].substring(4, 6);
 
-      DateTime now = DateTime.parse(datetime);
-      now = now.add(
-          Duration(hours: int.parse(offset), minutes: int.parse(offset_mnt)));
+        DateTime now = DateTime.parse(datetime);
+        now = now.add(
+            Duration(hours: int.parse(offset), minutes: int.parse(offset_mnt)));
+        isMorning = now.hour > 6 && now.hour < 20 ? true : false;
 
-      isMorning = now.hour > 6 && now.hour < 20 ? true : false;
-
-      time = DateFormat.jm().format(now);
+        time = DateFormat.jm().format(now);
+      } else {
+        isMorning = false;
+        throw Exception('Error at the moment');
+      }
     } catch (e) {
       print("Error occured: $e");
       time = "Cannot Display Time Due to Error Occured";
